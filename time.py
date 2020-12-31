268eca70232771b5c67d503aadffe8d43ffe58de
zfqxuz <71256494+zfqxuz@users.noreply.github.com>
2020-12-31 21:17:05 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,16 +1,19 @@
-import time
 import os
 import datetime
 import threading
 from docx import Document
+import time
+import pyttsx3
 
 global namelist
 namelist=[]
+
 def body(path):
     os.chdir(path)
     curr=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f"开始监视路径{path}，开始时间{curr}")
     n=os.listdir()
+
     while True:
         time.sleep(1)
         m=os.listdir()
@@ -20,7 +23,6 @@ def body(path):
             dif1=set(m).difference(set(n))
             dif2=set(n).difference(set(m))
             n=m
-            name=""
             curr=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             if dif1!=set():
                 for item in dif1:
@@ -29,6 +31,7 @@ def body(path):
                         continue
                     namelist.insert(0,name)
                     print(f"{curr},新增了{name}")
+
             if dif2!=set():
                 for item in dif2:
                     name=item
@@ -36,8 +39,11 @@ def body(path):
                         continue
                     print(f"{curr},移除了{name}")
 
+
 def change(k):
     print(k)
+    engine = pyttsx3.init()
+    engine.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ZH - CN_HUIHUI_11.0")
     while True:
         try:
             if len(namelist)==0:
@@ -53,13 +59,18 @@ def change(k):
                     f= open (name,"a")
                     f.write(" ")
                     f.close()
+                engine.say(name+"转换成功")
+                engine.runAndWait()
         except:
+            engine.say("转换失败")
+            engine.runAndWait()
             print("转换失败")
             continue
 
 if __name__ == '__main__':
+
     while True:
-        i=input("输入地址")
+        i=r"YourAddress"
         try:
             os.chdir(i)
             break
